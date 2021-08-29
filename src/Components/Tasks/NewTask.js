c89d4c9d8ad01917bb970f93c584cdde9e21b6ed
Joanna <jkwiatkowska@pulsartec.com>
2021-08-29 12:11:15 +0200

    adding state to the form values: task name and task category, reset form feature

--- src/Components/Tasks/NewTask.js
+++ src/Components/Tasks/NewTask.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Input from "../UI/Input";
 import Button from "../UI/Button";
@@ -6,17 +6,38 @@ import styles from "./NewTask.module.css"
 
 const NewTask = (props) => {
 
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredCategory, setEnteredCategory] = useState('');
+
+
     const addTaskHandler = (event) => {
-        event.preventDefault();
+        event.preventDefault(); 
+
+        //Instead of printing the value to the console we will need to call a function to add it to the list
+        console.log(enteredName, enteredCategory);
+
+        setEnteredName('');
+        setEnteredCategory('');
+
+    };
+
+    const taskNameChangeHandler = (event) => {
+        setEnteredName(event.target.value);
     }
+
+    const categoryChangeHandler = (event) => {
+        setEnteredCategory(event.target.value);
+    }
+
+
 
 
     return (
         <Card className={styles.form}>
-            <form>
-                <Input id="task_name" type="text" name="task_name">Name:</Input>
-                <Input id="task_category" type="text" name="task_category">Category:</Input>
-                <Button type="submit" onClick={addTaskHandler}>Start</Button>
+            <form onSubmit={addTaskHandler}>
+                <Input id="task_name" type="text" name="task_name" value={enteredName} onChange={taskNameChangeHandler}>Name:</Input>
+                <Input id="task_category" type="text" name="task_category" value={enteredCategory} onChange={categoryChangeHandler}>Category:</Input>
+                <Button type="submit">Start</Button>
             </form>
         </Card>
     )
